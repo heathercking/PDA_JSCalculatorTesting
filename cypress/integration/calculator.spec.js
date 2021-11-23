3b713c41ec3bc277573d2baad2e96e288308b3a4
Heather King <hkingdevelopment@gmail.com>
2021-11-23 16:32:20 +0000

    adds passing UI tests for updating display on number button and arithmetical operators

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -7,4 +7,18 @@ describe("Calculator", () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
   })
+
+  it('should update the display of the running total when number buttons are pressed', () => {
+    cy.get('#number3').click();
+    cy.get('#number4').click();
+    cy.get('#running-total').should('contain', '34');
+  })
+
+  it('should update the display with a result when arithmetical operations are selected', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('#running-total').should('contain', '6');
+  })
 })
